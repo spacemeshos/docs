bd1464bd9c911022cbf6c3f0d5a49839810d40c8
yaelmhoffman <56118594+yaelmhoffman@users.noreply.github.com>
2022-08-08 21:31:36 +0300

    Switching to permanent dark mode and changing logo to match

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -58,6 +58,11 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
+      colorMode: {
+    defaultMode: 'dark',
+    disableSwitch: true,
+    respectPrefersColorScheme: false,
+  },
       navbar: {
         title: 'Documentation Demo',
         logo: {
