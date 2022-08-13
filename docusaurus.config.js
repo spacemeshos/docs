d00d63e3fbf1916522d2c723a8c70f1d57b79ebf
yaelmhoffman <56118594+yaelmhoffman@users.noreply.github.com>
2022-08-14 00:52:09 +0300

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,8 +8,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'Spacemesh Documentation',
   tagline: 'All the docs are belong to you',
-  url: 'https://spacemeshos.github.io',
-  baseUrl: '/docs/',
+  url: 'https://platform.spacemesh.io',
+  baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.png',
@@ -131,7 +131,7 @@ const config = {
                      {
                               to: 'devzone',
                               label: 'Dev Zone',
-                          
+
                               }]
 
         },
