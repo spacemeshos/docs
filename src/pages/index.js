5dda1ea10029b0e95f667011d3f67a4f3ded78d4
yaelmhoffman <56118594+yaelmhoffman@users.noreply.github.com>
2024-02-26 22:15:56 +0200

    landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,57 +10,35 @@ import styles from './index.module.css';
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
+
     <header>
       <div className="container landing-container">
-        <h1 className="hero__title">Welcome to the Spacemesh documentation hub!</h1>
-        <p>This page is still a work-in-progress, as Team Spacemesh is hard at work creating in-depth, comprehensive documentation. In the meantime, you can learn the basics of Spacemesh and browse the guides available to get started on the Spacemesh Network.</p>
+       <h1 className="hero__title">Welcome to the Spacemesh documentation hub!</h1>
+       <p class='landing-description'>This page is still a work-in-progress, as Team Spacemesh is hard at work creating in-depth, comprehensive documentation. In the meantime, you can learn the basics of Spacemesh and browse the guides available to get started on the Spacemesh Network.</p>
 
-        </div>
+      <div className={styles.buttons}>
+        <Link
+          className="intro-card"
+          to="docs/learn/spacemesh/">
+          <div class='intro-card-title'>SPACEMESH 101</div>
+          <div class='intro-card-content'>Learn the basics about Spacemesh.</div>
+        </Link>
+        <Link
+          className="intro-card"
+          to="docs/start/">
+          <div class='intro-card-title'>GUIDES</div>
+          <div class='intro-card-content'>Learn how join the Spacsemesh testnet network as a smesher.</div>
+        </Link>
+
+      </div>
+
+
+      </div>
+
       </header>
-        // <div className={styles.buttons}>
-        //   <Link
-        //     className="intro-card"
-        //     to="docs/learn/spacemesh/">
-        //     <div class='intro-card-title'>INTRODUCTION</div>
-        //     <div class='intro-card-content'>Learn the basics about Spacemesh.</div>
-        //   </Link>
-        //   <Link
-        //     className="intro-card"
-        //     to="docs/start/">
-        //     <div class='intro-card-title'>GET STARTED</div>
-        //     <div class='intro-card-content'>Join the Spacsemesh testnet network as a smesher.</div>
-        //   </Link>
-        //
-        //   <Link
-        //     className="intro-card-inactive"
-        //     to="docs/specs/consensus/construction-cons/">
-        //     <div class='intro-card-title'>DOCUMENTATION</div>
-        //     <div class='intro-card-content'>This section is currently under construction.</div>
-        //   </Link>
-        //
-        //
-        // </div>
-        // <div className={styles.buttons}>
-        // <Link
-        //   className="intro-card-inactive"
-        //   to="docs/ecosystem/construction-eco/">
-        //   <div class='intro-card-title'>TOOLING</div>
-        //   <div class='intro-card-content'>This section is currently under construction.</div>
-        // </Link>
-        //   <Link
-        //     className="intro-card-inactive"
-        //     to="roadmap">
-        //     <div class='intro-card-title'>ROADMAP</div>
-        //     <div class='intro-card-content'>This section is currently under construction.</div>
-        //   </Link>
-        //
-        //   <Link
-        //     className="intro-card-inactive"
-        //     to="devzone">
-        //     <div class='intro-card-title'>DEV ZONE</div>
-        //     <div class='intro-card-content'>This section is currently under construction.</div>
-        //   </Link>
-        // </div>
+
+
+
   );
 }
 
